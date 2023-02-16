66121e8a1f1e46d11e69f53d5bda7c6a513f98cc
Neo <Trudymahlangu@outlook.com>
2023-02-16 22:24:13 +0200

    4 puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/**
+ * main - A program that prints a line with puts function
+ * return: 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
