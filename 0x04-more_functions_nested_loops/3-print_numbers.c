882d07d1229faffbf85090b814b3f5a84c2b3100
Neo <Trudymahlangu440@outlook.com>
2023-02-24 01:08:22 +0200

    3.The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * print_numbers - checks for a digit (0 through 9)
+ * print_numbers - checks for checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
@@ -12,4 +13,5 @@ void print_numbers(void)
 	{
 		_putchar(c);
 	}
+	_putchar('\n');
 }
